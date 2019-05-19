73e4eaf755809f40623679ef1d00f771f6c4fd41
Volkan GENÇ <gencvolkan@gmail.com>
2019-05-19 09:01:59 -0700

    Add project files.

---
+++ WebApplication7/Controllers/DemoProfilingController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Profiling;
+using System.Threading;
+using System.Web.Http;
+
+namespace WebApplication7.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DemoProfilingController : ControllerBase
+    {
+         
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string url1 = string.Empty;
+            string url2 = string.Empty;
+
+            using (MiniProfiler.Current.Step("Get Method"))
+            {
+                using (MiniProfiler.Current.CustomTiming("SQL", "SELECT * FROM Config"))
+                {
+                    Thread.Sleep(500);
+                    url1 = "https://google.com/";
+                    url2 = "https://stackoverflow.com/";
+
+                }
+            }
+            using (MiniProfiler.Current.Step("Use data for http call"))
+            {
+                using (MiniProfiler.Current.CustomTiming("HTTP", "GET" + url1))
+                {
+                    var client = new System.Net.WebClient();
+                    var reply = client.DownloadString(url1);
+                }
+                using (MiniProfiler.Current.CustomTiming("HTTP", "GET" + url2))
+                {
+                    var client = new System.Net.WebClient();
+                    var reply = client.DownloadString(url1);
+                }
+
+            }
+
+            return new string[] { "value1", "value2" };
+        }
+
+        public void GetFive()
+        {
+            int a = 10;
+        }
+    }
+}
---
+++ WebApplication7/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.StaticFiles;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+
+namespace WebApplication7
+{
+    public class Startup
+    {
+        private   IHostingEnvironment CurrentEnviroment { get; set; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            CurrentEnviroment = env;
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(env.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+
+            Configuration = builder.Build( );
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (CurrentEnviroment.IsDevelopment() || CurrentEnviroment.IsStaging() || CurrentEnviroment.EnvironmentName == "TEST")
+            {
+                services.AddMiniProfiler(options => options.RouteBasePath = "/profiler");
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info()
+                    {
+                        Description = "Benim Apim",
+                        Title = "v1",
+                        Version = "v1"
+
+                    });
+                });
+            }
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment() || env.IsStaging() || env.EnvironmentName == "TEST")
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+
+                    c.RoutePrefix = "api-doc";
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
+                    c.IndexStream = () => GetType().GetTypeInfo().Assembly.GetManifestResourceStream("WebApplication7.SwaggerIndex.html"); 
+                });
+
+                app.UseDeveloperExceptionPage();
+            }
+            else if (env.IsProduction())
+            {
+
+            }
+
+            app.UseMiniProfiler();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseMvc();
+            app.UseCors();
+        }
+    }
+}
